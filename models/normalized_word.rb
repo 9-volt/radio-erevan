a1c423c1500a2ceb82920b2f04a2f953b08c6d52
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-06-09 14:13:58 +0300

    Add racai flags to normalized word

--- models/normalized_word.rb
+++ models/normalized_word.rb
@@ -4,4 +4,5 @@ class NormalizedWord
   property :id,               Serial    # An auto-increment integer key
   property :initial_form,     String    # A varchar type string, for short strings
   property :normalized_form,  String    # A text block, for longer string data.
+  property :racai_flags,      String    # A text block, for longer string data.
 end
