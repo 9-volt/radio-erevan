0e1ddcb37d03ea0a7e723194eee484d49cccf74a
Ivan Zarea <zarea.ion@gmail.com>
2013-06-09 17:48:53 +0300

    :bowling: Added a restriction to URLs

--- db_setup.rb
+++ db_setup.rb
@@ -1,10 +1,16 @@
 require 'data_mapper'
 
-DataMapper::Logger.new($stdout, :debug)
+#DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/erevan')
 
-#require all models
+#require everything!
 Dir[File.dirname(__FILE__) + "/models/*.rb"].each {|f| require f }
+
+#require jobs
+Dir[File.dirname(__FILE__) + "/jobs/*.rb"].each {|f| require f }
+
+#require launchers
+Dir[File.dirname(__FILE__) + "/launchers/*.rb"].each {|f| require f }
 
 #finalize
 DataMapper.finalize
--- models/url.rb
+++ models/url.rb
@@ -2,7 +2,7 @@ class URL
   include DataMapper::Resource
 
   property :id,         Serial
-  property :url,        String
+  property :url,        String, unique: true
   property :source,     String    # String for now, need to include the association
   property :parsed,     Boolean, default: false, index: true
 end
