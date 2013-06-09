3915e20924e89d055cfb43153abc19341855f973
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-06-09 14:41:14 +0300

    Remove the useless model

--- models/normalized_word.rb
+++
@@ -1,8 +0,0 @@
-class NormalizedWord
-  include DataMapper::Resource
-
-  property :id,               Serial    # An auto-increment integer key
-  property :initial_form,     String    # A varchar type string, for short strings
-  property :normalized_form,  String    # A text block, for longer string data.
-  property :racai_flags,      String    # A text block, for longer string data.
-end
