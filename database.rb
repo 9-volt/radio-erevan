25e5aac79916d24863b3ea31a5f22e676456f921
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-06-09 13:42:50 +0300

    Added the db connection, nowmalized_word model

---
+++ database.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'data_mapper' # requires all the gems listed above
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/erevan')
+
+#require all models
+Dir[File.dirname(__FILE__) + "/models/*.rb"].each {|f| require f }
+
+#finalize
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
