15d03df9bd1385c99dee484f859145a359af216e
Ivan Zarea <zarea.ion@gmail.com>
2013-06-09 17:03:46 +0300

    :innocent: Added the unimedia fetcher

---
+++ parsing/fetchers/unimedia.rb
@@ -0,0 +1,24 @@
+require_relative '../parsers'
+
+module Fetchers end
+
+class Fetchers::Unimedia
+  def self.fetch(url)
+    unless url.parsed?
+      article = Parsers::Unimedia.fetch(url.url)
+      a = Article.new(source:    article.source,
+                      time:      article.time,
+                      author:    article.author,
+                      category:  article.category,
+                      url:       article.url,
+                      title:     article.title)
+
+      article.sentences.each do |s|
+        Sentence.create(text: s.text, article: a)
+      end
+
+      url.update(parsed: true)
+    end
+  end
+end
+
