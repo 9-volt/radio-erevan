873ecf81e1a37438875bfeab6dc2cee052c4171e
Ivan Zarea <zarea.ion@gmail.com>
2013-06-09 15:05:31 +0300

    :older_man: Forgot to add _spec

---
+++ spec/parsers/unimedia_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Parsers::Unimedia do
+  describe '.fetch' do
+    it 'parses the article properly' do
+      result = Parsers::Unimedia.fetch(fixtures_path + '/unimedia_article.html')
+
+      result.source.should == 'unimedia'
+      result.category.should == 'Politic'
+      result.title.should match /Tănase/u
+      result.sentences.should_not be_empty
+    end
+  end
+end
