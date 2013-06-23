b5380a0a652db2e6921bcfacdec10b88e314ea37
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 16:29:25 +0300

    :cyclone: Launching both sources at the same time

--- launchers/job_launcher.rb
+++ launchers/job_launcher.rb
@@ -2,9 +2,12 @@ require_relative '../db_setup'
 
 class JobLauncher
   def self.launch!
-    URL.all(parsed: false, error: false).take(10).each do |url|
-      ParseJob.perform_async(url.id)
-      puts "Parsing URL #{url.url}"
+    sources = %w(unimedia publika)
+    sources.each do |source|
+      URL.all(parsed: false, source: source, error: false).take(10).each do |url|
+        ParseJob.perform_async(url.id)
+        puts "Parsing URL #{url.url}"
+      end
     end
   end
 end
