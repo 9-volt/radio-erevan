c846d2073a1a6c18f1e2624ae52a8262232f7ce5
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 13:29:04 +0300

    :crocodile: A new and simple parse job

---
+++ jobs/parse_job.rb
@@ -0,0 +1,20 @@
+require 'sidekiq'
+require_relative '../db_setup'
+require 'lumberjack'
+
+
+class ParseJob
+  include Sidekiq::Worker
+  logger = Lumberjack::Logger.new("logs/sidekiq.log")
+
+  def perform(url_id)
+    url = URL.get(url_id)
+    begin
+      ArticleFetcher.fetch(url)
+    rescue Exception => e
+      logger.error(url)
+      logger.error(e)
+      url.update(error: true)
+    end
+  end
+end
