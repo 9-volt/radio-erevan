ba2e67d038132d22737b3a05cd07ad4580c3263d
Ivan Zarea <zarea.ion@gmail.com>
2013-06-23 12:17:14 +0300

    :briefcase: Moved the creation of the article to a base class

--- parsing/fetchers/unimedia.rb
+++ parsing/fetchers/unimedia.rb
@@ -2,21 +2,11 @@ require_relative '../parsers'
 
 module Fetchers end
 
-class Fetchers::Unimedia
+class Fetchers::Unimedia < Fetchers::Base
   def self.fetch(url)
     unless url.parsed?
       article = Parsers::Unimedia.fetch(url.url)
-      a = Article.create(source:    article.source,
-                         time:      article.time,
-                         author:    article.author,
-                         category:  article.category,
-                         url:       article.url,
-                         title:     article.title)
-
-      article.sentences.each do |s|
-        Sentence.create(text: s.text, article: a)
-      end
-
+      dump_article! article
       url.update(parsed: true)
     end
   end
