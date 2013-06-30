bd4c415f18e69b6861aa472a64d10c2ee40cab06
Ivan Zarea <zarea.ion@gmail.com>
2013-06-30 22:12:55 +0300

    Added a simple script to fetch the xmls

---
+++ scripts/jurnal-fetcher/script.py
@@ -0,0 +1,11 @@
+import urllib2
+import datetime as dt
+
+page = urllib2.urlopen('http://jurnal.md/ro/rss.xml')
+string = page.read()
+
+filename = dt.datetime.now().strftime('%Y-%m-%d.xml')
+f = open(filename, 'w')
+f.write(string)
+f.close
+
