457b0a5db1f70eccb50baddb938ae691542f1fe6
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-07-09 18:54:24 +0300

    Set env in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+ENV['EREVAN_ENV'] ||= 'test'
+
 require 'rspec'
 require 'factory_girl'
 require 'database_cleaner'
@@ -9,24 +11,4 @@ require_relative '../parsing/parsers'
 
 def fixtures_path
   File.expand_path('spec/fixtures/')
-end
-
-RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
 end
