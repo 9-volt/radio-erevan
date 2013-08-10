0056d2e9b4677c0f63b91cb2d68f22b4c281226a
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-08-10 16:44:39 +0300

    Rename fixture

--- spec/parsers/inprofunzime_spec.rb
+++ spec/parsers/inprofunzime_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Parsers::Inprofunzime do
   describe '.fetch' do
     it 'parses the article properly' do
-      result = Parsers::Inprofunzime.fetch(fixtures_path + '/protv_article.html')
+      result = Parsers::Inprofunzime.fetch(fixtures_path + '/inprofunzime_article.html')
 
       result.time = Time.new(2013, 'jul', 9, 16, 42)
       result.source.should == 'inprofunzime'
