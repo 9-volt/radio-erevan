fb97f774c4221a8b97203b621ce612410a70a1d2
Ivan Zarea <zarea.ion@gmail.com>
2013-08-10 19:32:54 +0300

    :cop: Protv adapter now has class<<self

--- parsing/url-updaters/protv.rb
+++ parsing/url-updaters/protv.rb
@@ -11,47 +11,50 @@ module URLUpdaters
       LINKS_PARTIALLY_SAVED = 2
     end
 
-    def update!
-      CATEGORIES.each {|category| update_links_for_category!(category) }
-    end
+    class << self
 
-    def update_links_for_category! category, page=1
-      puts "updating category #{category} page #{page}"
+      def update!
+        CATEGORIES.each {|category| update_links_for_category!(category) }
+      end
 
-      document = Nokogiri::HTML(open(archive_page(category, page)))
-      links = fetch_links document
+      def update_links_for_category! category, page=1
+        puts "updating category #{category} page #{page}"
 
-      if links.any?
-        status = save_links! links
+        document = Nokogiri::HTML(open(archive_page(category, page)))
+        links = fetch_links document
 
-        if status == SAVE_STATUS::LINKS_SAVED
-          update_links_for_category! category, page + 1
+        if links.any?
+          status = save_links! links
+
+          if status == SAVE_STATUS::LINKS_SAVED
+            update_links_for_category! category, page + 1
+          end
         end
       end
-    end
 
-    def save_links! links
-      puts "saving #{links.inspect}"
+      def save_links! links
+        puts "saving #{links.inspect}"
 
-      already_saved_links = URL.all :conditions => ['url in ?', links]
+        already_saved_links = URL.all :conditions => ['url in ?', links]
 
-      links.each {|url| URL.first_or_create :source => 'protv', :url => url }
+        links.each {|url| URL.first_or_create :source => 'protv', :url => url }
 
-      if already_saved_links.any?
-        SAVE_STATUS::LINKS_PARTIALLY_SAVED
-      else
-        SAVE_STATUS::LINKS_SAVED
+        if already_saved_links.any?
+          SAVE_STATUS::LINKS_PARTIALLY_SAVED
+        else
+          SAVE_STATUS::LINKS_SAVED
+        end
       end
-    end
 
-    def fetch_links document
-      document.css('.numeStire a#navigare').map do |e|
-        e.attributes['href'].value rescue nil
-      end.compact
-    end
+      def fetch_links document
+        document.css('.numeStire a#navigare').map do |e|
+          e.attributes['href'].value rescue nil
+        end.compact
+      end
 
-    def archive_page (category, page=1)
-      "http://protv.md/arhiva/#{category}/pagina-#{page}.html"
+      def archive_page (category, page=1)
+        "http://protv.md/arhiva/#{category}/pagina-#{page}.html"
+      end
     end
   end
 end
