900c37144c9776ff55135d7d01c16a2cd2d4dcec
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-08-12 21:49:56 +0300

    Sleep 1 sec between each job to avoid bans

--- launchers/job_launcher.rb
+++ launchers/job_launcher.rb
@@ -5,6 +5,7 @@ class JobLauncher
     sources = %w(unimedia publika jurnal protv)
     sources.each do |source|
       URL.all(parsed: false, source: source, error: false).take(10).each do |url|
+        sleep 1
         ParseJob.perform_async(url.id)
         puts "Parsing URL #{url.url}"
       end
