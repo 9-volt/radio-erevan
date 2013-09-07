8b1a0662ce78ffaf62a7f323921406bc8b22bd1f
Bumbu VM <bumbuvm@wat.com>
2013-09-08 01:32:28 +0300

    Avoid duplicate enqueue

--- launchers/job_launcher.rb
+++ launchers/job_launcher.rb
@@ -1,12 +1,22 @@
 require_relative '../db_setup'
 
 class JobLauncher
+  QUEUE = Sidekiq::Queue.new
+
+  def self.url_scheduled? id
+    QUEUE.any? {|job| job.args.first == id }
+  end
+
   def self.launch!
     sources = %w(unimedia publika jurnal protv timpul)
     sources.each do |source|
       URL.all(parsed: false, source: source, error: false).take(1).each do |url|
-        puts "Scheduling #{url.url}"
-        ParseJob.perform_async(url.id)
+        if url_scheduled? url.id
+          puts "Url #{url.url} is already scheduled for parsing"
+        else
+          puts "Scheduling #{url.url}"
+          ParseJob.perform_async(url.id)
+        end
       end
     end
   end
