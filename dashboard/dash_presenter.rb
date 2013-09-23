dd58b6c89d480647aa967b355910a34eb336b724
Ivan Zarea <izarea@nebulent.com>
2013-09-23 10:03:49 +0300

    :cop: A dashboard presenter

---
+++ dashboard/dash_presenter.rb
@@ -0,0 +1,28 @@
+class DashPresenter
+  attr_reader :urls
+
+  def initialize
+    @urls = URL.all
+  end
+
+  def all
+    urls.count
+  end
+
+  def parsed
+    urls.count(parsed: true)
+  end
+
+  def parsed_percentage
+    parsed / all.to_f * 100
+  end
+
+  def per_source
+    Hash[*URL.aggregate(:source, :all.count).flatten]
+  end
+
+  def per_source_parsed
+    Hash[*URL.aggregate(:source, :all.count, conditions: ['parsed = ?', true]).flatten]
+  end
+end
+
